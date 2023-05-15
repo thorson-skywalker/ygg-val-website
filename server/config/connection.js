d91b153d65b759e8c6da2a2bddec62ce3d0be9b9
Charles <cchester1@angelo.edu>
2023-05-14 20:34:33 -0500

    db connection successful. authentication method applied

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,38 @@
+// dotenv dependency loads env variables
+// sequelize dependency compiles connection code for backend connection to db
 const dotenv = require('dotenv');
 const Sequelize = require('sequelize');
 
+// env variables loaded
 dotenv.config();
 
-let sequelize = function (node_env) {
-      if(node_env === 'local') {
-            sequelize = new Sequelize(proces.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-                  host: 'localhost',
-                  dialect: 'mysql',
-                  define: {
-                        freezeTableName: true,
-                        timestamps: true
-                  }
-            })
-      } else {
-            console.error('new error')
-      }
-};
+// stage allocation for sequelize variable
+let sequelize;
+
+// conditional use of sequelize variable and subsequent db connection according to env variables loaded
+if (process.env.NODE_ENV === 'local') {
+      sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            define: {
+                  freezeTableName: true,
+                  timestamps: true
+            }
+      })
+
+      console.log('connected to local db')
+} else {
+      console.error('server error at db connection time in connection file')
+}
+
+exports.sequelize = sequelize;
+exports.connection = () => {
+      sequelize.authenticate()
+      .then(() => {
+            console.log('Authenticating..')
+      })
+      .catch(err => {
+            console.log('Error while authenticating db connection')
+            console.error(err)
+      })
+}
--- server/server.js
+++ server/server.js
@@ -1,19 +1,42 @@
-// server dependencies
+// server dependency and initiation of express dependency
 const express = require('express')
+const app = express()
+
+// dotenv dependency for loading env variables and load of env variables subsequently
+const dotenv = require('dotenv')
+dotenv.config()
+
+// import db connection file
+const sequelize = require('./config/connection.js')
+
+// if a web service is not found, app will run locally on PORT 4001 @ (localhost:4001)
+const PORT = process.env.PORT
+
+// library used to write paths to below files in below routes
 const path = require('path')
 
-// initiate instance of express
-const app = express()
-// if a web service is not found, app will run locally on PORT 4001 @ (localhost:4001)
-const PORT = process.env.PORT || 4001;
-
+// static routes to frontend html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/layouts/home.html'))
 });
-
 app.get('/api/heroimage', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/assets/images/hero_2.jpeg'))
 })
 
+sequelize.connection()
 // written to finally run express and recieve a callback
-app.listen(PORT, () => console.log('App listening on PORT ' + PORT));
+sequelize.sequelize.sync()
+.then(() => {
+    if(process.env.NODE_ENV==='local') {
+        app.listen(4001, () => {
+            console.log('running locally on PORT 4001')
+        })
+    } else {
+        console.error('server error at runtime')
+    }
+})
+.catch(err => {
+    if(err) {
+        throw new Error(err)
+    }
+})
